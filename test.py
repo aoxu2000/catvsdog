79dde596b3b8cf704aedbfb4ff673a1ec19d19cd
xuao <xuao575@gmail.com>
2024-09-05 22:43:33 +0800

    update

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 import pandas as pd
 from tqdm import tqdm  # 导入 tqdm
-from model.resnet import ResNet50
+from model.resnet import ResNet152
 from utils.loadtest import CatsDogsTestDataset
 
 
 def load_model(model_path, device='cuda'):
-    model = ResNet50(num_classes=2)  # 假设我们有2个类别：猫和狗
+    model = ResNet152(num_classes=2)  # 假设我们有2个类别：猫和狗
     model.load_state_dict(torch.load(model_path, map_location=device))
     model = model.to(device)
     model.eval()  # 设置为评估模式
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     # 解析命令行参数
     parser = argparse.ArgumentParser(description="Predict using a pre-trained ResNet model.")
     parser.add_argument('--test_dir', type=str, required=False, default='./dogs-vs-cats-redux-kernels-edition/test', help='Path to the test dataset directory.')
-    parser.add_argument('--model_path', type=str, required=False, default='./ckpt/resnet50_cats_dogs.pth', help='Path to the trained model (.pth file).')
+    parser.add_argument('--model_path', type=str, required=False, default='./ckpt/resnet152_cats_dogs.pth', help='Path to the trained model (.pth file).')
     parser.add_argument('--batch_size', type=int, default=4, help='Batch size for testing.')
     args = parser.parse_args()
 
